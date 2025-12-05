3919853a470a82e96f54ca3001dbe2c98fda7480
Can Sücüllü <cansucullu@gmail.com>
2025-12-05 12:46:37 +0000

    Add NumPy test file

---
+++ np_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print(np.random.rand())
+
+print(np.pi)
+
+x = np.linspace(0, 10, 1000)
+
+print("type(x) is ", type(x))
+print("len(x) is ", len(x))
+print("x.shape is ", x.shape)
+print("x is ", x)
